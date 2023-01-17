03d6313d275f6f05545d42f54ec257e9bab46e5f
Nabnit Jha <nabnitcjha@gmil.com>
2023-01-17 23:23:55 +0545

    improve code

---
+++ resources/js/components/dashboard/sidebar/ParentSidebar.vue
@@ -0,0 +1,11 @@
+<template>
+    <ul class="sidebar-nav" id="sidebar-nav">
+<li class="nav-item">
+  <a class="nav-link collapsed hand"  @click.stop="$root.changeRoute('/student')">
+    <i class="bi bi-person"></i>
+    <span>Student</span>
+  </a>
+</li><!-- End Student Nav -->
+
+</ul>
+</template>
