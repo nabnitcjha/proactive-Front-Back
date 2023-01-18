de83aba4f926b57e5445ecd62483c3a05561dd96
Nabnit Jha <nabnitcjha@gmil.com>
2023-01-18 10:27:58 +0545

    improve code

--- resources/js/components/detail/TeacherClasses.vue
+++ resources/js/components/detail/TeacherClasses.vue
@@ -1,140 +1,82 @@
 <template>
     <div class="row">
         <div class="col-lg-12">
-            <div class="card" v-if="mode == 'fetch-subjects'">
+            <div
+                class="card"
+                style="width: 18rem"
+                v-for="(class_info, index) in sorted_class"
+                :key="index"
+            >
                 <div class="card-body">
-                    <div class="add-item">
-                        <h5 class="card-title">Subject List</h5>
-                        <button
-                            type="button"
-                            class="btn btn-add"
-                            @click.stop="$root.changeRoute('/add-subject')"
-                        >
-                            <i class="bi bi-plus"></i> ADD SUBJECT
-                        </button>
-                    </div>
-                    <hr class="hr-color" />
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="card" v-else>
-                <div class="card-body">
-                    <div class="add-item">
-                        <h5 class="card-title">Subject Info</h5>
-                        <button
-                            type="button"
-                            class="btn btn-back"
-                            @click.stop="$router.go(-1)"
-                        >
-                            BACK
-                        </button>
-                    </div>
-                    <hr class="hr-color" />
-                    <form class="row g-3 needs-validation" novalidate>
-                        <div class="col-md-4">
-                            <label for="subject_first_name" class="form-label"
-                                ><img :src="icons.First_name" />&nbsp;First
-                                name</label
+                    <h5 class="card-title">
+                        {{ class_info.topic }}
+                    </h5>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            ><span
+                                class="badge rounded-pill text-bg-warning mr-2"
+                                v-for="(
+                                    cls_selected_day, index
+                                ) in class_info.selected_day"
+                                :key="index"
+                                >{{ findDay(cls_selected_day) }}</span
                             >
-                            <input
-                                type="text"
-                                class="form-control"
-                                id="subject_first_name"
-                                required
-                                v-model="subject.name"
-                            />
-                            <!-- <div class="valid-feedback">
-                Looks good!
-              </div> -->
-                            <div class="invalid-feedback">
-                                Please choose a fist name.
-                            </div>
-                        </div>
-
-                        <div class="col-12">
-                            <button
-                                class="btn btn-success"
-                                @click.stop="addSubject"
-                            >
-                                Save
-                            </button>
-                        </div>
-                    </form>
-                    <!-- End Custom Styled Validation -->
+                        </li>
+                        <li class="list-group-item">
+                          {{ class_info.duration }}
+                        </li>
+                        <li class="list-group-item">
+                            <b>{{ timeFormater(class_info.start_date) }}{{ ' - ' }}{{ {{ timeFormater(class_info.end_date) }} }}</b>
+                        </li>
+                    </ul>
+                    <a
+                        class="btn btn-success"
+                        @click.stop="setClassId(class_info.id)"
+                        >Go TO Class</a
+                    >
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<style>
-body {
-    padding: 1rem;
-}
-</style>
-
 <script>
-import { First_name } from "../../Assets/formIcons/index";
+import { loginInfoStore } from "../../stores/loginInfo";
+import { mapState } from "pinia";
 export default {
     data() {
         return {
-            icons: {
-                First_name: First_name,
-            },
-            subject: {
-                name: "",
-            },
-            subjects: [],
+            sorted_class: [],
         };
     },
-    props: {
-        mode: String,
+    computed: {
+        ...mapState(loginInfoStore, ["getLoginInfo"]),
     },
     methods: {
-        callBack() {
-            this.save();
-        },
-        addSubject() {
-            this.checkValidation(this.callBack);
-        },
-        async save() {
-            let formData = new FormData();
+        async getClasses() {
+            let urlText =
+                "teacher/" + this.getLoginInfo.teacher_info.id + "/sortedClass";
 
-            formData.append("subject_info[name]", this.subject.name);
-            let postResponse = {};
-            let urlText = "addSubject";
-
-            postResponse = await this.post(urlText, formData);
-            this.subject = { name: "" };
-            this.$router.push({ name: "subject" });
+            getResponse = await this.get(urlText, 0, true);
+            this.sorted_class = getResponse.data.data;
         },
-        async getSubjects() {
-            let urlText = "getSubjects";
-
-            postResponse = await this.get(urlText,0,true);
+        findDay(day) {
+            switch (day) {
+                case "0":
+                    return "Sun";
+                case "1":
+                    return "Mon";
+                case "2":
+                    return "Tues";
+                case "3":
+                    return "Wed";
+                case "4":
+                    return "Thurs";
+                case "5":
+                    return "Fri";
+                case "6":
+                    return "Sat";
+            }
         },
     },
 };
