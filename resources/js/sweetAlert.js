c3d46d9f9c8d00412a337e99959de9f65143dead
Nabnit Jha <nabnitcjha@gmil.com>
2023-02-08 01:46:45 +0545

    improve code

--- resources/js/sweetAlert.js
+++ resources/js/sweetAlert.js
@@ -9,7 +9,8 @@ export default {
                 .fire({
                     title: "Are you sure?",
                     text: "You won't be able to revert this!",
-                    imageUrl: 'https://media3.giphy.com/media/VD4Bt6FyYWcWj0LzDK/giphy.gif?cid=ecf05e474e7l1aq7styn2ewhpwhb551w672171bduai3ppol&rid=giphy.gif&ct=g',
+                    icon: 'warning',
+                    // imageUrl: 'https://media3.giphy.com/media/VD4Bt6FyYWcWj0LzDK/giphy.gif?cid=ecf05e474e7l1aq7styn2ewhpwhb551w672171bduai3ppol&rid=giphy.gif&ct=g',
                     imageWidth: 400,
                     imageHeight: 200,
                     imageAlt: 'Custom image',
